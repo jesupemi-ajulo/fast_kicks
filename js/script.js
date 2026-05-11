d4316ea7e75ab74709f134addb6fa92b0a8f5348
jesupemi-ajulo <jesupemii19@gmail.com>
2026-05-11 23:07:08 +0100

    signin page

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
-const swiper = new Swiper('.myswiper', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+// const swiper = new Swiper(".myswiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+// const tswiper = new Swiper(".testimonialSwiper", {
+//   slidesPerView: 2,
+//   spaceBetween: 10,
+
+//   loop: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// const harmburger = document.querySelector("#harmburger");
+// const mobileNavLink = document.querySelector("#mobile-nav-links");
+// const navOverlay = document.querySelector("#navOverlay");
+// harmburger.addEventListener("click", () => {
+//   mobileNavLink.classList.toggle("open");
+//   navOverlay.classList.toggle("open");
+// });
+// navOverlay.addEventListener("click", () => {
+//   mobileNavLink.classList.remove("open");
+//   navOverlay.classList.remove("open");
+// });
+
+// signup
+
+const siContainer = document.querySelector("#siContainer");
+const signUp = document.querySelector("#fcSignup");
+const signIn = document.querySelector("#fcSignin");
+signUp.addEventListener("click", () => {
+  siContainer.classList.add("active");
+  console.log('hi')
 });
-const tswiper = new Swiper(".testimonialSwiper", {
-  slidesPerView: 2,
-  spaceBetween: 10,
-
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+signIn.addEventListener("click", () => {
+  siContainer.classList.remove("active");
 });
-
-
-
-
-const harmburger=document.querySelector('#harmburger')
-const mobileNavLink=document.querySelector('#mobile-nav-links')
-const navOverlay=document.querySelector('#navOverlay')
-harmburger.addEventListener('click',()=>{
-mobileNavLink.classList.toggle('open')
-navOverlay.classList.toggle('open')
-})
-navOverlay.addEventListener('click',()=>{
-  mobileNavLink.classList.remove('open')
-  navOverlay.classList.remove('open')
-})
